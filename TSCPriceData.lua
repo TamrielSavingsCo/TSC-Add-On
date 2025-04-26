d3e6b5ef2b3418d047fe18f8e34541a2f8bb278f
emoran0403 <emoran0403@gmail.com>
2025-04-26 10:28:54 -0400

    feat: added data for testing

--- TSCPriceData.lua
+++ TSCPriceData.lua
@@ -12,8 +12,42 @@ TSCPriceDataInfo = {
 -- This table contains price data for standard items
 TSCPriceData = {
     -- Materials
-    [30164] = { price = 200 }, -- Columbine
-    [77585] = { price = 50 },  -- Flax (Not Really LOL)
-    [23126] = { price = 15 },  -- Iron Ore (Not Really LOL)
-    -- Other non-gear items...
+    [34349] = { price = 123 },  -- Acai Berry
+    [115026] = { price = 123 }, -- Aetherial Dust
+    [120894] = { price = 123 }, -- Animus Stone
+    [34311] = { price = 123 },  -- Apples
+    [33755] = { price = 123 },  -- Bananas
+    [34329] = { price = 123 },  -- Barley
+    [34309] = { price = 123 },  -- Beets
+    [27059] = { price = 123 },  -- Bervez Juice
+    [34334] = { price = 123 },  -- Bittergreen
+    [34320] = { price = 123 },  -- Bug Parts
+    [34324] = { price = 123 },  -- Carrots
+    [27057] = { price = 123 },  -- Cheese
+    [33772] = { price = 123 },  -- Coffee
+    [33768] = { price = 123 },  -- Comberry
+    [34323] = { price = 123 },  -- Corn
+    [171326] = { price = 123 }, -- Cyrodiil Citrus
+    [120078] = { price = 123 }, -- Diminished Aetherial Dust
+    [33753] = { price = 123 },  -- Fish
+    [27100] = { price = 123 },  -- Flour
+    [26802] = { price = 123 },  -- Frost Mirriam
+    [28609] = { price = 123 },  -- Game
+    [26954] = { price = 123 },  -- Garlic
+    [27052] = { price = 123 },  -- Ginger
+    [34346] = { price = 123 },  -- Ginkgo
+    [34347] = { price = 123 },  -- Ginseng
+    [28604] = { price = 123 },  -- Greens
+    [34333] = { price = 123 },  -- Guarana
+    [27043] = { price = 123 },  -- Honey
+    [34316] = { price = 123 },  -- Honey Comb
+    [27035] = { price = 123 },  -- Isinglass
+    [33771] = { price = 123 },  -- Jasmine
+    [28610] = { price = 123 },  -- Jazbay Grapes
+    [27049] = { price = 123 },  -- Lemon
+    [34330] = { price = 123 },  -- Lotus
+    [34308] = { price = 123 },  -- Melon
+    [27048] = { price = 123 },  -- Metheglin
+    [27064] = { price = 123 },  -- Millet
+    [33773] = { price = 123 },  -- Mint
 }
