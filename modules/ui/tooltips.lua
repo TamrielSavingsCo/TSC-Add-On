04e8374a1dd86d84acfc8f00b82e9d5905ba404e
emoran0403 <emoran0403@gmail.com>
2025-05-10 13:19:04 -0400

    chore: added better checks to tooltip

--- modules/ui/tooltips.lua
+++ modules/ui/tooltips.lua
@@ -4,9 +4,15 @@ local Tooltips = {}
 function Tooltips.AddPriceToGamepadTooltip(tooltipObject, tooltipType, itemLink)
     if tooltipType ~= GAMEPAD_LEFT_TOOLTIP then return end
 
-    local itemName = itemLink and GetItemLinkName(itemLink)
-    if not (tooltipObject and itemName and itemName ~= "") then return end
+    -- Looser item link check: match any |H#:item: link
+    if type(itemLink) ~= "string" or not itemLink:find("^|H%d:item:") then return end
 
+    local itemName = GetItemLinkName(itemLink)
+    if not itemName or itemName == "" then return end
+
+    -- Only skip if item type is ITEMTYPE_NONE (most non-items)
+    local itemType = GetItemLinkItemType(itemLink)
+    if itemType == ITEMTYPE_NONE then return end
 
     local tooltip = tooltipObject:GetTooltip(tooltipType)
     if not tooltip then return end
